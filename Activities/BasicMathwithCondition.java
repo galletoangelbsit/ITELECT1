2d13785f1024345edf4ebe98705b044ca6200744
AngelGalleto_IT3B <galletoangel_bsit@plmun.edu.ph>
2025-08-19 09:21:04 +0800

    Add files via upload

---
+++ Activities/BasicMathwithCondition.java
@@ -0,0 +1,48 @@
+//Angel Galleto
+//BSIT3B
+//Activity: 1 Basic Math with Condition
+public class BasicMathwithCondition
+{
+    public static void main(String[] args) {
+
+        int num1 = 56;
+        int num2 = 55;
+        int choice = 4;
+        int result;
+
+        System.out.println("1st number: " + num1);
+        System.out.println("2nd number: " + num2);
+        System.out.println("arithmeticprocess: " + choice);
+
+        //if else condition and arithmetic process
+
+        //addition
+          if (choice == 1) {
+            result = num1 + num2;
+            System.out.println("Result (Addition): " + result);
+        } else if (choice == 2) {
+            result = num1 - num2;
+            
+        //Subtraction
+            System.out.println("Result (Subtraction): " + result);
+        } else if (choice == 3) {
+            result = num1 * num2;
+            
+        //Multiplication    
+            System.out.println("Result (Multiplication): " + result);
+            
+            
+        //Division
+        } else if (choice == 4) {
+            if (num2 != 0) {
+                result = num1 / num2;
+                System.out.println("Result (Division): " + result);
+            } else {
+                System.out.println("Error: Division by zero will kill us you fool");
+            }
+        } else {
+            System.out.println("Invalid choice! Please select 1-4.");
+        }
+
+    }
+}
